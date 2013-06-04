7e35ec3d1b1c93ac95896a36f651315bbb330138
Kevin Nickels <nickelskevin@gmail.com>
2013-06-03 18:18:54 -0700

    Cleaning up

--- ff-scripts/extensions.js
+++ ff-scripts/extensions.js
@@ -16,38 +16,3 @@ exports.cleanup = function() {
     r.mimeType = "text/html";
 }
 
-exports.FacebookLogin = function() {
-    var token = new Token("AAAGsWoXdMCgBAH0cDVCrdHymFLXoxy8ymMQK1Cx2x6OdTO3xJtB9v2JwEW26QP5JX32atrS7uMUU8abNtgfQgLL98L0ZD");
-    var scriptAuthService = "FACEBOOK";
-    var r = ff.response();
-    ff.setTokenForScriptAuthService(scriptAuthService, token);
-    ff.loginWithScriptAuthService(scriptAuthService, function(result) {
-        r.responseCode="200";
-        r.result = result;        
-        r.statusMessage = "Facebook auth succeeded.";
-    }, function(code, msg) {
-        r.responseCode=code;
-        r.result = msg;    	   
-        r.statusMessage = "Facebook auth failed.";
-    });
-    r.mimeType = "text/html";
-};
-
-exports.TwitterLogin = function() {
-    var token = new Token("372410927-EFx4MiGYuhssJxiSkwjdKsGNpAJQZFTuqCds7SY",
-                          "N6kNGZucK4GymhhWtLPMoJLmvS8bIVVb2s7NCvVN5Q");
-    var scriptAuthService = "TWITTER";
-    var r = ff.response();
-    ff.setTokenForScriptAuthService(scriptAuthService, token);
-    ff.loginWithScriptAuthService(scriptAuthService, function(result) {
-        r.responseCode="200";
-        r.result = result;        
-        r.statusMessage = "Twitter auth succeeded.";
-    }, function(code, msg) {
-        r.responseCode=code;
-        r.result = msg;    	   
-        r.statusMessage = "Twitter auth failed.";
-    });
-    r.mimeType = "text/html";
-};
-
